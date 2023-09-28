1d0850508dea67e0741807d90329341fe8427155
rah1595 <113435252+rah1595@users.noreply.github.com>
2023-09-28 11:49:39 +0530

    Update MenuItems.php

--- src/Models/MenuItems.php
+++ src/Models/MenuItems.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Harimayco\Menu\Models;
+namespace Vfixtechnology\Menu\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -33,11 +33,11 @@ class MenuItems extends Model
 
     public function parent_menu()
     {
-        return $this->belongsTo('Harimayco\Menu\Models\Menus', 'menu');
+        return $this->belongsTo('Vfixtechnology\Menu\Models\Menus', 'menu');
     }
 
     public function child()
     {
-        return $this->hasMany('Harimayco\Menu\Models\MenuItems', 'parent')->orderBy('sort', 'ASC');
+        return $this->hasMany('Vfixtechnology\Menu\Models\MenuItems', 'parent')->orderBy('sort', 'ASC');
     }
 }
